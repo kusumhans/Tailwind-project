f556691ae39f22a57ab9e6b52cbbc6b93f1cd337
kusumhans <hanswork007@gmail.com>
2026-01-17 13:25:35 +0530

    responsive Tailwind CSS project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],//"*" when we want use tailwind for all files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
